e20e8cfc26ccd031bb962ff7f33ad11f71065bb9
Amol-R <amolrindhe.qa1@gmail.com>
2024-10-21 17:58:56 +0530

    Added in Branch-1

---
+++ cypress/e2e/My Test/Tc01.cy.js
@@ -0,0 +1,9 @@
+///<reference types = "cypress"/>
+
+describe('Test Suit', ()=>{
+
+    it('First test',()=>{
+
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    })
+})
