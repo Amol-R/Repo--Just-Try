56d2436c6ee273c35451c798218c29121446d648
Amol-R <amolrindhe.qa1@gmail.com>
2024-10-21 18:08:08 +0530

    first chanes in code

--- cypress/e2e/My Test/Tc01.cy.js
+++ cypress/e2e/My Test/Tc01.cy.js
@@ -5,5 +5,6 @@ describe('Test Suit', ()=>{
     it('First test',()=>{
 
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Amol')
     })
 })
