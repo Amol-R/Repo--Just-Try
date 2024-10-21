1d6c8b60dd78b39a43a0872040c8b903091f4fbb
Amol-R <amolrindhe.qa1@gmail.com>
2024-10-21 18:13:34 +0530

    Delete cypress/e2e/My Test directory
    nothging

--- cypress/e2e/My Test/Tc01.cy.js
+++
@@ -1,9 +0,0 @@
-///<reference types = "cypress"/>
-
-describe('Test Suit', ()=>{
-
-    it('First test',()=>{
-
-        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    })
-})
