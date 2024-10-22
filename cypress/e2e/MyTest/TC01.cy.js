3b28f9ce45eb778e22bfaf4a780249925311d799
Amol-R <amolrindhe.qa1@gmail.com>
2024-10-22 14:55:33 +0530

    aa

--- cypress/e2e/MyTest/TC01.cy.js
+++ cypress/e2e/MyTest/TC01.cy.js
@@ -7,6 +7,6 @@ describe('Test Suit', ()=>{
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
         cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Amol')
         cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Amol-12345')
-        cy.get('.oxd-button').
+        cy.get('.oxd-button').c
     })
 })
