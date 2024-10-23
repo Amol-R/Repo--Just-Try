b6127f66a628568a210cf623ada3cc29fd7367f5
Amol-R <amolrindhe.qa1@gmail.com>
2024-10-23 09:04:03 +0530

    aaa

--- cypress/e2e/MyTest/Tc02.cy.js
+++ cypress/e2e/MyTest/Tc02.cy.js
@@ -1,6 +1,8 @@
 ///<reference types = "cypress"/>
 
+describe('Test Suit', ()=>{
 
+    it('First test',()=>{
 
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
         cy.get('[name="username"]').type('Amol')
