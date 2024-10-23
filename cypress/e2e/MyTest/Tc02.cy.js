7b280d9d198ffb0b8b154690391bafa2e4def51a
Amol-R <amolrindhe.qa1@gmail.com>
2024-10-23 09:06:04 +0530

    Update Tc02.cy.js
    revers change

--- cypress/e2e/MyTest/Tc02.cy.js
+++ cypress/e2e/MyTest/Tc02.cy.js
@@ -1,14 +1,11 @@
 ///<reference types = "cypress"/>
 
 describe('Test Suit', ()=>{
-
     it('First test',()=>{
-
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
         cy.get('[name="username"]').type('Amol')
         cy.get('[name="password"]').type('Amol-12345')
         cy.get('.oxd-button').click()
-
-        
+       
     })
 })
